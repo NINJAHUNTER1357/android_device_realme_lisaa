ccc835c6744414fa0a1c0e85527340085f3f18dc
Thundergod_Thor <81786979+NINJAHUNTER1357@users.noreply.github.com>
2026-01-20 01:15:52 +0000

    lisaa: Update vendorsetup.sh for required patches

---
+++ vendorsetup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+## MTK GED KPI support to fix broken Mediatek gpufreq
+cd frameworks/native  
+git fetch https://github.com/NINJAHUNTER1357/android_frameworks_native.git
+git cherry-pick 27d45c28c9286f412ffdd68a3030a504c3be57fb
+## Fix the udfps dimlayer on MediaTek HWComposer
+git cherry-pick 2ea1b7b5306ea1713dbcff1541b74d36f97536ae
