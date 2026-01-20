ee95e5e4ed8585516caf6080216a242fdd8bfdb8
Thundergod_Thor <81786979+NINJAHUNTER1357@users.noreply.github.com>
2026-01-20 00:35:48 +0000

    Lisaa: Ufps experiment Fix the udfps dimlayer on MediaTek HWComposer Right now the entire screen dither to dark except for the dim layer better than getting flashbanged at HBM level but still not the expected behavior.

--- device.mk
+++ device.mk
@@ -126,8 +126,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Fingerprint
-$(call soong_config_set_bool,surfaceflinger,has_mtk_udfps,true)
-$(call soong_config_set,surfaceflinger,mtk_dim_layer,OnScreenFingerprintPressedIcon)
+$(call soong_config_set,surfaceflinger,has_mtk_udfps,true)
 
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.oplus
@@ -233,8 +232,7 @@ PRODUCT_PACKAGES += \
     ApertureResTarget \
     LineageSDKResTarget \
     LineageSettingsProviderResTarget \
-    OplusDozeResTarget \
-    LineageSystemUIResTarget
+    OplusDozeResTarget 
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
--- extract-files.py
+++ extract-files.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
-#
 # SPDX-FileCopyrightText: 2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -68,7 +67,17 @@ blob_fixups: blob_fixups_user_type = {
      'vendor/lib64/libcustomer_cameradata.so'): blob_fixup()
         .add_needed('libprocessgroup_shim.so'),
     'vendor/lib64/hw/hwcomposer.mtk_common.so': blob_fixup()
-        .add_needed('libprocessgroup_shim.so'),
+        .add_needed('libprocessgroup_shim.so')
+        #.binary_regex_replace(
+        #    b'OnScreenFingerprintDimLayer',
+        #    b'SurfaceView[UdfpsController'
+        #)
+        .binary_regex_replace(
+            b'OnScreenFingerprintPressedIcon',
+            b'SurfaceView[UdfpsControllerOve'
+    ),
+    # CHECK IF YOU NEED TO PATCH BOTH 'OnScreenFingerprintXXXXXX' OR ONLY ONE OF THEM
+    # AND IF YOU NEED TO ENABLE MTK UDFPS NATIVE WITH BOTH, ONLY ONE OF THEM OR WITH NONE
     'vendor/lib64/hw/mt6895/android.hardware.camera.provider@2.6-impl-mediatek.so': blob_fixup()
         .add_needed('libcamera_metadata_shim.so')
         .replace_needed('libutils.so', 'libutils-v32.so'),
